0e98c4bb5a202980b85acacbf3fc08deefeb582c
Raúl Marty <santor3medio@gmail.com>
2018-03-07 17:24:10 -0600

    #1. Add true links to footer.php

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
     <div>
       <div>
         <a href="index.html">
-          <img class="site-footer-logo" src="<? echo get_template_directory_uri() . '/img/logos/wicore-blue-logo.png' ?>">
+          <img class="site-footer-logo" src="<?php echo get_template_directory_uri() . '/img/logos/wicore-blue-logo.png' ?>">
         </a>
       </div>
       <span class="site-footer-text">©Copyright, Wicore.<br>2018</span>
@@ -48,13 +48,13 @@
           <h1 class="site-footer-titles nunitosans-black">Conócenos</h1>
         </div>
         <div class="site-footer-text">
-          <a href="servicios.html">
+          <a href="http://wicore.mx/servicios.html">
             <span class="site-footer-link">Servicios</span>
           </a>
-          <a href="nosotros.html">
+          <a href="http://wicore.mx/nosotros.html">
             <span class="site-footer-link">Nosotros</span>
           </a>
-          <a href="contacto.html">
+          <a href="http://wicore.mx/contacto.html">
             <span class="site-footer-link">Contacto</span>
           </a>
         </div>
@@ -66,12 +66,12 @@
         <div class="site-footer-row">
           <div class="site-footer-column">
             <a href="https://www.linkedin.com/company/wicoremx/" target="_blank">
-              <img class="site-footer-icon" src="<? echo get_template_directory_uri() . '/img/icons/in-icon.png' ?>">
+              <img class="site-footer-icon" src="<?php echo get_template_directory_uri() . '/img/icons/in-icon.png' ?>">
             </a>
           </div>
           <div>
             <a href="">
-              <img class="site-footer-icon" src="<? echo get_template_directory_uri() . '/img/icons/fb-icon.svg' ?>">
+              <img class="site-footer-icon" src="<?php echo get_template_directory_uri() . '/img/icons/fb-icon.svg' ?>">
             </a>
           </div>
         </div>
